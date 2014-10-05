31ccf3c8e21de26650b040f5c43ce8d1cd69b380
melvinhernandez <m.hernandez@berkeley.edu>
2014-10-04 21:09:01 -0700

    HW1 Done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,5 @@
 class StaticPagesController < ApplicationController
+  def about
+  	@me = User.find(7)
+  end
 end
---
+++ db/migrate/20141005032003_removeyears_old_from_user.rb
@@ -0,0 +1,4 @@
+class RemoveyearsOldFromUser < ActiveRecord::Migration
+  def change
+  end
+end
